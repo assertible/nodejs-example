67e5b6d706fe2bca84e9f2a57387bfb12bece154
Cody Reichert <cody@reichertbrothers.com>
2017-05-04 17:04:29 -0500

    Set explicity Content-Type header on index route
    Express is using a text/html Content-Type, but we're only returning a
    plain text string. This sets an explicit Content-Type header in the
    response for the "/" route so that clients can interpret it correctly.

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ app.set('port', (process.env.PORT || 5000))
 
 /* Handler for "/" */
 app.get('/', function(request, response) {
-  response.send('Hello, world!')
+    response.setHeader('Content-Type', 'text/plain')
+    response.send('Hello, world!')
 })
 
 
 /* Initialize the server */
 const server = app.listen(app.get('port'), function() {
-  console.log("app is running at localhost:" + app.get('port'))
+    console.log("app is running at localhost:" + app.get('port'))
 })
 
 module.exports = server
